36b2d3e4d904270afff898aa96046793fb9b0481
Jonas Natzer <jonas.natzer@celus.io>
2024-09-15 19:34:02 +0200

    add proper layout

--- keyboards/celus/keymaps/default/keymap.c
+++ keyboards/celus/keymaps/default/keymap.c
@@ -5,8 +5,23 @@
 #include "lib/logos.h"
 #include "lib/wpm.h"
 
+enum layer_number {
+    _QWERTY = 0,
+    _LOWER,
+    _RAISE,
+    _ADJUST,
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+    /* [QWERTY] = LAYOUT( */
+    /*     KC_A,    KC_B, */
+    /*     KC_C,    KC_D */
+    /* ) */
+
+    // TODO missing KC_LBRC and KC_RBRC
     /*
+     * QWERTY
      * ┌───┬───┬───┬───┬───┬───┐   ┌───┬───┬───┬───┬───┬───┐
      * │ESC│ 1 │ 2 │ 3 │ 4 │ 5 │   │ 6 │ 7 │ 8 │ 9 │ 0 │ ` │
      * ├───┼───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┼───┤
@@ -14,23 +29,83 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ├───┼───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┼───┤
      * │L2 │ A │ S │ D │ F │ G │   │ H │ J │ K │ L │ : │ ' │
      * ├───┼───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┼───┤
-     * │   │ Z │ X │ C │ V │ B │   │ N │ M │ , │ . │ / │   │
+     * │   │ Z │ X │ C │ V │ B │   │ N │ M │ , │ . │ / │BSP│
      * ├───┼───┼───┼───┼───┴───┼───┼───┴───┼───┼───┼───┼───┤
      * │CTR│GUI│ALT│SFT│  BSP  │L3 │  SPC  │ENT│L1 │ALT│DEL│
      * └───┴───┴───┴───┴───────┴───┴───────┴───┴───┴───┴───┘
      */
-    [0] = LAYOUT(
-        KC_A,    KC_B,
-        KC_C,    KC_D
-    )
+  [_QWERTY] = LAYOUT(
+        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,           KC_6,    KC_7, KC_8,    KC_9,    KC_0, KC_GRV,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,           KC_Y,    KC_U, KC_I,    KC_O,    KC_P, KC_MINS,
+        MO(2),   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,           KC_H,    KC_J, KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+        KC_NO,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,           KC_N,    KC_M, KC_COMM, KC_DOT,  KC_SLSH, KC_BSPC,
+        KC_LCTL, KC_LGUI, KC_LALT, KC_LSFT,          KC_BSPC, MO(3), KC_SPC,        KC_ENT,  MO(1),   KC_RALT, KC_DEL,
+        ),
+    /*
+     * LOWER
+     * ┌───┬───┬───┬───┬───┬───┐   ┌───┬───┬───┬───┬───┬───┐
+     * │F1 │F2 │F3 │F4 │F5 │F6 │   │F7 │F8 │F9 │F10│F11│F12│
+     * ├───┼───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┼───┤
+     * │   │   │   │   │   │   │   │   │   │UP │ [ │ ] │ \ │
+     * ├───┼───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┼───┤
+     * │   │ ( │ ) │ { │ } │PREV│ │NEXT│LT │DN │RT │ - │ = │
+     * ├───┼───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┼───┤
+     * │   │ ! │ @ │ # │ $ │ % │   │ ^ │ & │ * │ ( │ ) │   │
+     * ├───┼───┼───┼───┼───┴───┼───┼───┴───┼───┼───┼───┼───┤
+     * │   │   │   │   │  DEL  │   │       │   │   │   │   │
+     * └───┴───┴───┴───┴───────┴───┴───────┴───┴───┴───┴───┘
+     */
+  [_LOWER] = LAYOUT(
+        KC_F1, KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,         KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, KC_F12,
+        KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,         KC_NO,   KC_NO,   KC_UP,   KC_LBRC, KC_RBRC, KC_BSLS,
+        KC_NO, KC_LPRN, KC_RPRN, KC_LCBR, KC_RCBR, KC_MPRV,       KC_MNXT, KC_LEFT, KC_DOWN, KC_RGHT, KC_MINS, KC_EQL,
+        KC_NO, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,       KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_NO,
+        KC_NO, KC_NO,   KC_NO,   KC_NO,            KC_DEL, KC_NO, KC_NO,            KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        ),
+    /*
+     * _RAISE
+     * ┌───┬───┬───┬───┬───┬───┐   ┌───┬───┬───┬───┬───┬───┐
+     * │   │   │   │   │   │   │   │   │   │   │   │   │   │
+     * ├───┼───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┼───┤
+     * │   │ 7 │ 8 │ 9 │   │   │   │   │   │   │   │   │   │
+     * ├───┼───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┼───┤
+     * │   │ 4 │ 5 │ 6 │   │VOL+│ │VOL-│ ( │ ) │ { │ } │ = │
+     * ├───┼───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┼───┤
+     * │   │ 1 │ 2 │ 3 │   │   │   │   │   │   │   │   │   │
+     * ├───┼───┼───┼───┼───┴───┼───┼───┴───┼───┼───┼───┼───┤
+     * │   │   │ 0 │ . │  DEL  │ENT│       │   │   │   │   │
+     * └───┴───┴───┴───┴───────┴───┴───────┴───┴───┴───┴───┘
+     */
 
-    /* [1] = LAYOUT( */
-    /*     KC_X,   KC_X,   KC_X,   KC_X,   KC_X,   KC_X,       KC_X,   KC_X,   KC_X,   KC_X,   KC_X,   KC_X, */
-    /*     KC_X,   KC_X,   KC_X,   KC_X,   KC_X,   KC_X,       KC_X,   KC_X,   KC_X,   KC_X,   KC_X,   KC_X, */
-    /*     KC_X,   KC_X,   KC_X,   KC_X,   KC_X,   KC_X,       KC_X,   KC_X,   KC_X,   KC_X,   KC_X,   KC_X, */
-    /*     KC_X,   KC_X,   KC_X,   KC_X,   KC_X,   KC_X,       KC_X,   KC_X,   KC_X,   KC_X,   KC_X,   KC_X, */
-    /*     KC_X,   KC_X,   KC_X,   KC_X,       KC_X,     KC_X,     KC_X,       KC_X,   KC_X,   KC_X,   KC_X */
-    /* ) */
+  [_RAISE] = LAYOUT(
+        KC_NO, KC_NO, KC_NO, KC_NO,  KC_NO,  KC_NO,          KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        KC_NO, KC_7,  KC_8,  KC_9,   KC_NO,  KC_NO,          KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        KC_NO, KC_4,  KC_5,  KC_6,   KC_NO,  KC_VOLD,        KC_VOLU, KC_LPRN, KC_RPRN, KC_LCBR, KC_RCBR, KC_EQL,
+        KC_NO, KC_1,  KC_2,  KC_3,   KC_NO,  KC_NO,          KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        KC_NO, KC_NO, KC_0,  KC_DOT,         KC_DEL, KC_ENT, KC_NO,            KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        ),
+    /*
+     * This layer is not used currently
+     * _ADJUST
+     * ┌───┬───┬───┬───┬───┬───┐   ┌───┬───┬───┬───┬───┬───┐
+     * │   │   │   │   │   │   │   │   │   │   │   │   │   │
+     * ├───┼───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┼───┤
+     * │   │   │   │   │   │   │   │   │   │   │   │   │   │
+     * ├───┼───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┼───┤
+     * │   │   │   │   │   │   │   │   │   │   │   │   │   │
+     * ├───┼───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┼───┤
+     * │   │   │   │   │   │   │   │   │   │   │   │   │   │
+     * ├───┼───┼───┼───┼───┴───┼───┼───┴───┼───┼───┼───┼───┤
+     * │   │   │   │   │       │   │       │   │   │   │   │
+     * └───┴───┴───┴───┴───────┴───┴───────┴───┴───┴───┴───┘
+     */
+    [_ADJUST] = LAYOUT(
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
+        KC_NO, KC_NO, KC_NO, KC_NO,        KC_NO, KC_NO, KC_NO,        KC_NO, KC_NO, KC_NO, KC_NO
+    )
 };
 
 
